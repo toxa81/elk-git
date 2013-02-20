3af250142d66f86b7a07780745b36e2ebd376518
toxa81 <toxa81@gmail.com>
2013-02-20 17:18:12 +0100

    update printing of matrix elements

--- src/addons/expigqr/printmegqblh.f90
+++ src/addons/expigqr/printmegqblh.f90
@@ -11,9 +11,10 @@ character*100 fname
 write(fname,'("MEGQBLH_iq_",I4.4,".OUT")')iq
 if (mpi_grid_root((/dim_k/))) then
   open(200,file=trim(adjustl(fname)),form="FORMATTED",status="REPLACE")
+  write(200,'("q : ",3G18.10)')vql(:,iq)
   do ikloc=1,nkptnrloc
     ik=mpi_grid_map(nkptnr,dim_k,loc=ikloc)
-    write(200,'("ikloc : ",I4,"  idxkq : ",2I4)')ikloc,idxkq(:,ik)
+    write(200,'("ik : ",I4,"  jk : ",I4)')ik,idxkq(1,ik)
     do ist2=1,nstsv
       write(200,'("  ist2 : ",I4)')ist2
       do ist1=1,nstsv
